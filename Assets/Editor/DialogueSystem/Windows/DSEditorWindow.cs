97a897bb5b741a5759eaffbedda2b86bfc61e5fc
Armend <armend_tahiraga@outlook.com>
2024-10-19 15:51:24 +0200

    Changed the canvas scale and reference resolution, created the window for the dialogue editor and added a blank graph with some styling

---
+++ Assets/Editor/DialogueSystem/Windows/DSEditorWindow.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace DS.Windows {
+    public class DSEditorWindow : EditorWindow {
+        [MenuItem("Window/DS/Dialogue Graph")]
+        public static void ShowExample() {
+            GetWindow<DSEditorWindow>("Dialogue Graph");
+        }
+
+        private void OnEnable() {
+            AddGraphView();
+            AddStyles();
+        }
+
+        private void AddGraphView() {
+            DSGraphView graphView = new DSGraphView();
+            graphView.StretchToParentSize();
+            rootVisualElement.Add(graphView);
+        }
+
+        private void AddStyles() {
+            StyleSheet styleSheet = (StyleSheet) EditorGUIUtility.Load("DialogueSystem/DSVariables.uss");
+            rootVisualElement.styleSheets.Add(styleSheet);
+        }
+    }
+}
