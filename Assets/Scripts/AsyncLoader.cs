50326e694fff10f32825ab5633f41f5cb74ac51c
Armend <armend_tahiraga@outlook.com>
2024-11-13 23:44:19 +0100

    Finished the loading logic

---
+++ Assets/Scripts/AsyncLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AsyncLoader : MonoBehaviour {
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private Slider loadingSlider;
+
+    public void LoadLevelBtn(string levelToLoad) {
+        mainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadLevelAsync(levelToLoad));
+    }
+
+    IEnumerator LoadLevelAsync(string levelToLoad) {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
+
+        while (!loadOperation.isDone) {
+            float progress = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            loadingSlider.value = progress;
+            yield return null;
+        }
+    }
+}
