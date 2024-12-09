2b7e436b3173942feca6e195f8f42ed3a6e3bea2
Armend <armend_tahiraga@outlook.com>
2024-12-09 17:03:20 +0100

    Finished the menu

---
+++ Assets/SlimUI/Modern Menu 1/Scripts/CheckVolume.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SlimUI.ModernMenu{
+	public class CheckVolume : MonoBehaviour {
+		public void  Start (){
+			GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("Volume");
+		}
+
+		public void UpdateVolume (){
+			GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("Volume");
+		}
+	}
+}
